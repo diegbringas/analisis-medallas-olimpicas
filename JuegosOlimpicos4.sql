76a648eb952dd3446bba8eb2b0992ed0a818d63f
diegbringas <65004302+diegbringas@users.noreply.github.com>
2024-11-18 10:58:59 -0500

    Update JuegosOlimpicos4.sql

--- JuegosOlimpicos4.sql
+++ JuegosOlimpicos4.sql
@@ -1,21 +1,20 @@
--- Eliminar la tabla si ya existe
+4. Averiguar que pais gano la mayor cantidad de medallas de oro, plata y bronce durante cada Juego Olimpico
+
+
 IF OBJECT_ID('tablefunc', 'U') IS NOT NULL
     DROP TABLE tablefunc;
 
--- Crear la tabla 'tablefunc' y llenarla con los datos
 SELECT 
-    -- Extraer el nombre del juego (antes del guion '-')
     CASE 
         WHEN CHARINDEX('-', games_country) > 0 
         THEN SUBSTRING(games_country, 1, CHARINDEX('-', games_country) - 1)
-        ELSE games_country  -- Si no existe el guion, devuelve el valor completo (como juego)
+        ELSE games_country  
     END AS games, 
 
-    -- Extraer el país (después del guion '-')
     CASE 
         WHEN CHARINDEX('-', games_country) > 0 
         THEN SUBSTRING(games_country, CHARINDEX('-', games_country) + 1, LEN(games_country)) 
-        ELSE NULL  -- Si no existe el guion, asigna NULL a la columna de país
+        ELSE NULL  
     END AS country, 
 
     -- Contar medallas de oro, plata y bronce
@@ -25,22 +24,21 @@ SELECT
 INTO tablefunc
 FROM (
     SELECT 
-        CONCAT(games, '-', nr.region) AS games_country,  -- Concatenación de juegos y país
+        CONCAT(games, '-', nr.region) AS games_country,  
         Medal, 
         COUNT(1) AS total_medals
     FROM OlympicsKaggle..athlete_events$ oh
     JOIN OlympicsKaggle..noc_regions$ nr 
         ON nr.NOC = oh.NOC
-    WHERE Medal IN ('Gold', 'Silver', 'Bronze')  -- Filtramos solo medallas de oro, plata y bronce
+    WHERE Medal IN ('Gold', 'Silver', 'Bronze')  
     GROUP BY games, nr.region, Medal
 ) AS source
 PIVOT (
     SUM(total_medals)  -- Agregamos la suma de las medallas
-    FOR Medal IN ([Gold], [Silver], [Bronze])  -- Convertimos las medallas en columnas
+    FOR Medal IN ([Gold], [Silver], [Bronze])  
 ) AS pvt
 ORDER BY games_country;  
 
--- Consulta final para obtener el país con más medallas de oro por cada juego
 SELECT DISTINCT 
     games,
     CONCAT(
