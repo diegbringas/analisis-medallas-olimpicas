ba49065ab714e1839f903827a2687f4be3ca1774
diegbringas <65004302+diegbringas@users.noreply.github.com>
2024-11-18 11:02:05 -0500

    Update JuegosOlimpicos3.sql

--- JuegosOlimpicos3.sql
+++ JuegosOlimpicos3.sql
@@ -1,3 +1,5 @@
+3. Averiguar el total de medallas de oro, plata y bronce ganado por cada pais en las Olimpiadas.
+
 SELECT country, 
        ISNULL([Gold], 0) AS gold, 
        ISNULL([Silver], 0) AS silver, 
@@ -10,12 +12,13 @@ FROM (
     FROM OlympicsKaggle..athlete_events$ oh
     JOIN OlympicsKaggle..noc_regions$ nr 
         ON nr.NOC = oh.NOC
-    WHERE Medal IN ('Gold', 'Silver', 'Bronze')  -- Filtramos solo medallas de oro, plata y bronce
+    WHERE Medal IN ('Gold', 'Silver', 'Bronze')  
     GROUP BY nr.region, Medal
 ) AS source
+       
 PIVOT (
-    SUM(total_medals)  -- Agregamos la suma de las medallas
-    FOR Medal IN ([Gold], [Silver], [Bronze])  -- Convertimos las medallas en columnas
+    SUM(total_medals)  
+    FOR Medal IN ([Gold], [Silver], [Bronze])  
 ) AS pvt
 ORDER BY country;
 
